18f897594b682a06147a51e2c2304145abcb053b
Mike Odnis <airwolf635@gmail.com>
2025-03-08 19:23:54 -0500

    Quick commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,21 +6,13 @@ import { env } from "./env.mjs"
  * @type {import('next').NextConfig}
  */
 const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   logging: {
     fetches: {
       fullUrl: true,
     },
   },
   experimental: { instrumentationHook: true },
-  rewrites() {
-    return [
-      { source: "/healthz", destination: "/api/health" },
-      { source: "/api/healthz", destination: "/api/health" },
-      { source: "/health", destination: "/api/health" },
-      { source: "/ping", destination: "/api/health" },
-    ]
-  },
 })
 
 export default config
